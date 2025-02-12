3e896284146930a18899386df2f34b7db9cdbea6
mcgarret254 <mcgarretronald@gmail.com>
2025-02-12 12:13:27 +0300

    borrowing test

--- test/models/borrowing_test.rb
+++ test/models/borrowing_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class BorrowingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:test_user)  # Load the test user from fixtures
+    @book = books(:some_book)  # Load the test book from fixtures
+    @borrowing = Borrowing.new(user: @user, book: @book, due_date: Date.today + 7, returned: false)
+  end
+
+  test "should be valid" do
+    assert @borrowing.valid?
+  end
+
+  test "should require a user" do
+    @borrowing.user = nil
+    assert_not @borrowing.valid?
+  end
+
+  test "should require a book" do
+    @borrowing.book = nil
+    assert_not @borrowing.valid?
+  end
+
+  test "should have a due date" do
+    @borrowing.due_date = nil
+    assert_not @borrowing.valid?
+  end
+
+  test "default returned should be false" do
+    assert_not @borrowing.returned
+  end
 end
